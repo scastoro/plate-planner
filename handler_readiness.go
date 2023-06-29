de43b33435ef5983e42a8268beaded4eaa483449
Sal Castoro <salcastoro@gmail.com>
2023-06-29 16:31:58 -0500

    Add json response functions

--- handler_readiness.go
+++ handler_readiness.go
@@ -1,19 +1,9 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
 	"net/http"
 )
 
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	dat, err := json.Marshal(struct{}{})
-	if err != nil {
-		log.Println("Failed to marshal json")
-		w.WriteHeader(500)
-		return
-	}
-	w.Header().Add("Content-Type", "application/json")
-	w.WriteHeader(200)
-	w.Write(dat)
+	respondWithJson(w, 200, struct{}{})
 }
