d7cb63fd0a908d2043dae38645626a814a5b163e
Sal Castoro <salcastoro@gmail.com>
2023-06-30 17:12:46 -0500

    Update create to not set id

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -1,6 +1,6 @@
 -- name: CreateUser :one
-INSERT INTO "Admin"."Users" (id, first_name, last_name, body_weight, username, email, password, lastLoggedIn)
-values($1, $2, $3, $4, $5, $6, $7, $8)
+INSERT INTO "Admin"."Users" (first_name, last_name, body_weight, username, email, password, lastLoggedIn)
+values($1, $2, $3, $4, $5, $6, $7)
 RETURNING *;
 
 -- name: GetUserById :one
